7d9cc3cc6c3c5a5392f326f8eeb4a55c1c136840
tomcat <tomcat@devrabbit142>
2019-05-07 23:22:16 +0530

    HelloWorldAgain

---
+++ src/test/java/api/sqe_demo_may8/googleapi/apipageclasses/GoogleAPIGetDataset.java
@@ -0,0 +1,83 @@
+package api.sqe_demo_may8.googleapi.apipageclasses; 
+
+public class GoogleAPIGetDataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "sqe_demo_may8";
+
+ public static String primaryInfo  = "{\"module_description\":\"desc\","
+ + "\"mobile_platform\":null,"
+ + "\"report_upload_url\":\"http://192.168.1.142:80/UploadReportFile\","
+ + "\"repository_commit_message\":\"HelloWorldAgain\","
+ + "\"browser_type\":null,"
+ + "\"project_url\":\"https://maps.googleapis.com/\","
+ + "\"jenkins_username\":\"admin\","
+ + "\"testcase_id\":251,"
+ + "\"project_name\":\"SQE_Demo_May8\","
+ + "\"testset_name\":null,"
+ + "\"is_web\":false,"
+ + "\"jenkins_password\":\"admin\","
+ + "\"project_id\":283,"
+ + "\"framework_path\":\"/home/devrabbit/SmartQE/SmartQE-Users/7/Selenium/SQE_Demo_May8\","
+ + "\"sub_module_id\":0,"
+ + "\"repository_url\":\"https://dbc867eb7dd514cf4da6212b34f085f155179e95@github.com/devrabbititsol/SQE_Demo_May8.git\","
+ + "\"sub_module_description\":null,"
+ + "\"testcase_name\":\"TC1\","
+ + "\"executed_timestamp\":1557251534702,"
+ + "\"is_execute\":false,"
+ + "\"file_name\":null,"
+ + "\"testset_id\":0,"
+ + "\"jenkins_token\":\"11b2bb6ad5a0829d41a3149cea7bf2b07f\","
+ + "\"is_generate\":false,"
+ + "\"jenkins_url\":\"http://192.168.1.142:8081/\","
+ + "\"project_description\":\"desc\","
+ + "\"module_id\":395,"
+ + "\"user_id\":7,"
+ + "\"bundle_id\":null,"
+ + "\"sub_module_name\":null,"
+ + "\"device_os_version\":null,"
+ + "\"execution_environment\":\"jenkins\","
+ + "\"module_name\":\"GoogleAPI\"}";
+
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "TC1-Default";
+
+public static final String datasetResources1 = "maps/api/place/nearbysearch/json";
+public static final int requestType1 = 1;
+public static final int bodyType1 = -1;
+
+ public static String urlParams1  = "[{\"param_value\":\"-33.8670522,"
+ + "151.1957362\","
+ + "\"url_param_id\":813,"
+ + "\"param_key\":\"location\","
+ + "\"dataset_id\":1243,"
+ + "\"param_desc\":\"\"},"
+ + "{\"param_value\":\"restaurant\","
+ + "\"url_param_id\":814,"
+ + "\"param_key\":\"type\","
+ + "\"dataset_id\":1243,"
+ + "\"param_desc\":\"\"},"
+ + "{\"param_value\":\"cruise\","
+ + "\"url_param_id\":815,"
+ + "\"param_key\":\"keyword\","
+ + "\"dataset_id\":1243,"
+ + "\"param_desc\":\"\"},"
+ + "{\"param_value\":\"AIzaSyBhALFz5ldUASRKUXEhv1iXjXKE8OlULyk\","
+ + "\"url_param_id\":816,"
+ + "\"param_key\":\"key\","
+ + "\"dataset_id\":1243,"
+ + "\"param_desc\":\"\"},"
+ + "{\"param_value\":\"1500\","
+ + "\"url_param_id\":817,"
+ + "\"param_key\":\"radius\","
+ + "\"dataset_id\":1243,"
+ + "\"param_desc\":\"\"}]";
+
+ public static String headers1  = "[]";
+
+ public static String body1  = "{}";
+
+}
